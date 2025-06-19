2361f4e9197233a780612e08a791842748565c7f
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-06-18 22:39:55 -0800

    get_avarege added

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ class Student:
         self.name = name
         self.student_id = student_id
         self.__grades = {}  # private dictionary
+        
     def add_grade(self, subject, score):
         self.__grades[subject] = score
+        
+    def get_average(self):
+        if not self.__grades:
+            return 0.0
+        return sum(self.__grades.values()) / len(self.__grades)
